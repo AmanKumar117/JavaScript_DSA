84935e0f60118aac3aa47459b0ffb9ef128dd720
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-05-28 14:55:08 +0530

    added evenodd.js

---
+++ evenodd.js
@@ -0,0 +1,15 @@
+let fs = require("fs");
+let data = fs.readFileSync('./evenodd.txt', 'utf-8');
+let idx = 0;
+data = data.split('\n');
+function readLine() 
+{
+    idx++;
+    return data[idx - 1].trim();
+}
+///////////////////////////////////////////////////
+let num = parseInt(readLine());
+if(num % 2 == 0){
+    console.log("Even");
+}
+else{ console.log("Odd");}
